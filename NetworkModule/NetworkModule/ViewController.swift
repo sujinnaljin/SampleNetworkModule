912156c09bf37ef4df96727b2d9f67788274f719
sujin <sujikang@ebay.com>
2022-01-08 18:05:42 +0900

    Make Initial project

---
+++ NetworkModule/NetworkModule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkModule
+//
+//  Created by Kang, Su Jin (강수진) on 2022/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
